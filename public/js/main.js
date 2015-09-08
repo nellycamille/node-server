ccd8649a9183d3d598d7800b70f0322800798add
Nelly Camille <Camila@Edwars-MacBook-Pro-2.local>
2015-09-08 17:24:39 -0400

    primera prueba

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,63 @@ function getHTML(page) {
 $(document).ready(function(){
   $("#link-travesia").on("click", function() {
     getHTML('travesia.html');
-
+  });
+  $("#link-home").on("click", function() {
+    getHTML('home.html');
+  });
+  $("#link-contacto").on("click", function() {
+    getHTML('contact.html');
+  });
+  $("#link-bellie").on("click", function() {
+    getHTML('bellie.html');
+  });
+  $("#link-embarazo").on("click", function() {
+    getHTML('embarazo.html');
+  });
+  $("#link-blessings").on("click", function() {
+    getHTML('blessing.html');
+  });
+  $("#link-familia").on("click", function() {
+    getHTML('familia.html');
+  });
+  $("#link-comercial").on("click", function() {
+    getHTML('comercial.html');
+  });
+  $("#link-parto").on("click", function() {
+    getHTML('parto.html');
+  });
+  $("#link-posparto").on("click", function() {
+    getHTML('post-parto.html');
+  });
+  $("#link-lactancia").on("click", function() {
+    getHTML('lactancia.html');
+  });
+  $("#link-historia").on("click", function() {
+    getHTML('historia.html');
   });
 
-
 });
+
+
+
+///////////// This JS is for dropdown   //////////////////////
+
+( function( $ ) {
+$( document ).ready(function() {
+// Cache the elements we'll need
+var menu = $('#cssmenu');
+var menuList = menu.find('ul:first');
+var listItems = menu.find('li').not('#responsive-tab');
+
+// Create responsive trigger
+menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+
+// Toggle menu visibility
+menu.on('click', '#responsive-tab', function(){
+	listItems.slideToggle('slow');
+	listItems.addClass('collapsed');
+});
+});
+} )( jQuery );
+
+///////////// End dropdown   ////////////////////////////////
